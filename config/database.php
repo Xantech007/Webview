25408d593581278fe24305e46328f40259f4cc86
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2026-02-19 05:40:45 +0100

    Update database.php

--- config/database.php
+++ config/database.php
@@ -2,9 +2,9 @@
 class Database {
     public static function connect() {
         return new PDO(
-            "mysql:host=localhost;dbname=YOUR_DB;charset=utf8mb4",
-            "DB_USER",
-            "DB_PASS",
+            "mysql:host=sql304.infinityfree.com;dbname=if0_41120839_webcl;charset=utf8mb4",
+            "if0_41120839",
+            "v6OWzaMSbz",
             [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
