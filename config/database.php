89a906fa0f469b652e46d5d18e26d5120c1dee5c
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2026-02-21 08:11:50 +0100

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,14 +1,7 @@
 <?php
-class Database {
-    public static function connect() {
-        return new PDO(
-            "mysql:host=sql304.infinityfree.com;dbname=if0_41120839_webcl;charset=utf8mb4",
-            "if0_41120839",
-            "v6OWzaMSbz",
-            [
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-            ]
-        );
-    }
+$conn = new mysqli("sql304.infinityfree.com", "if0_41120839", "v6OWzaMSbz", "if0_41120839_webcl");
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
+?>
