8b535ddacc06c5bac1601268e4d4b2f50ef4efe8
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2026-02-21 08:07:56 +0100

    Create footer.php

---
+++ inc/footer.php
@@ -0,0 +1,10 @@
+<div class="footer">
+    <a href="index.php">Home</a>
+    <a href="#">Task</a>
+    <a href="#">Team</a>
+    <a href="#">VIP</a>
+    <a href="#">Me</a>
+</div>
+
+</body>
+</html>
