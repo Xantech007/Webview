d5e5b27bddd4bc6a58e8ab4472b35f39d86d8f10
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2026-02-23 06:59:35 +0100

    Update footer.php

--- inc/footer.php
+++ inc/footer.php
@@ -6,5 +6,22 @@
     <a href="#">Me</a>
 </div>
 
+<script>
+window.onload = function(){
+
+    var slides = document.querySelectorAll(".banner-slider img");
+    var current = 0;
+
+    if(slides.length < 2) return;
+
+    setInterval(function(){
+        slides[current].style.display = "none";
+        current = (current + 1) % slides.length;
+        slides[current].style.display = "block";
+    }, 1500);
+
+};
+</script>
+
 </body>
 </html>
